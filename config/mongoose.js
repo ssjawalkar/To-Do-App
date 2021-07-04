15854b5c75eb3992f96d2e629120fe8ae41308ea
samihan123 <samihan.jawalkar@gmail.com>
2021-07-05 00:03:03 +0530

    changing mongoose config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect('mongodb://localhost/Todo_DB');
 
 const db = mongoose.connection;
 
