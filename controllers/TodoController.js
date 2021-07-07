f1f3f4dd953620f7b7a740882e1b55152a643517
samihan123 <samihan.jawalkar@gmail.com>
2021-07-08 02:11:40 +0530

    adding css

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -3,8 +3,11 @@ const todo_list = require("../models/todo-list")
 module.exports.home = function(req, res){
     //console.log(req.cookies);
     //res.cookie('user_id', 25);
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     return res.render('home', {
-        title: "Home"
+        title: "Home",
+        today_date : String(date)
     });
 }
 
